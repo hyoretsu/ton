6e624e01382ea7796c5924cb552f59386be4fc02
Aran Leite <hyoretsu@gmail.com>
2022-08-30 23:41:00 -0300

    Mobile: envio de mensagens no Chat

--- mobile/src/screens/Chat.tsx
+++ mobile/src/screens/Chat.tsx
@@ -3,17 +3,34 @@ import { wait } from 'data/utils';
 import { format } from 'date-fns';
 import { useCallback, useEffect, useState } from 'react';
 import { FlatList } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Feather';
 
 import BottomBar from '@components/BottomBar';
 
-import { Container, MessageView, MessageTime, MessageText } from '@styles/Chat';
+import {
+  Container,
+  MessageView,
+  MessageTime,
+  MessageText,
+  UserInput,
+  InputView,
+  MessageSend,
+  DateSection,
+  DateSectionMark,
+  DateSectionText,
+} from '@styles/Chat';
 
 import answers from 'assets/messages.json';
 
 interface Message {
+  id: string;
   sender: string;
   body: string;
   time: Date;
+}
+
+interface DateDict {
+  [id: string]: string;
 }
 
 export interface ChatParams {
@@ -22,29 +39,36 @@ export interface ChatParams {
 
 const Chat: React.FC<RouteParams<ChatParams>> = ({ route }) => {
   const [messages, setMessages] = useState<Message[]>([]);
+  const [currentMessage, setCurrentMessage] = useState('');
+  const [dates, setDates] = useState<DateDict>({});
 
-  const answer = useCallback(async (message: string) => {
-    // @ts-ignore
-    const responses: string[] = answers[message];
+  const answer = useCallback(
+    async (message: string) => {
+      // @ts-ignore
+      const responses: string[] = answers[message];
 
-    responses.forEach(async (response, i) => {
-      await wait(2000 * (i + 1));
+      responses.forEach(async (response, i) => {
+        await wait(2000 * (i + 1));
 
-      setMessages(old => [
-        ...old,
-        {
-          sender: 'system',
-          body: response,
-          time: new Date(),
-        },
-      ]);
-    });
-  }, []);
+        setMessages(old => [
+          ...old,
+          {
+            id: String(messages.length - 1),
+            sender: 'system',
+            body: response,
+            time: new Date(),
+          },
+        ]);
+      });
+    },
+    [messages],
+  );
 
   useEffect(() => {
     if (route.params) {
       setMessages([
         {
+          id: String(messages.length - 1),
           sender: 'user',
           body: route.params.content,
           time: new Date(),
@@ -53,22 +77,66 @@ const Chat: React.FC<RouteParams<ChatParams>> = ({ route }) => {
 
       answer(route.params.content);
     }
-  }, [answer, route]);
+  }, [answer, messages, route]);
+
+  useEffect(() => {
+    messages.forEach(message => {
+      const formattedDate = format(message.time, 'dd/MM/yyyy');
+
+      if (!Object.values(dates).includes(formattedDate)) {
+        setDates(old => ({
+          ...old,
+          [message.id]: formattedDate,
+        }));
+      }
+    });
+  }, [dates, messages]);
+
+  const sendMessage = (): void => {
+    if (currentMessage) {
+      setMessages(old => [
+        ...old,
+        {
+          id: String(messages.length - 1),
+          sender: 'user',
+          body: currentMessage,
+          time: new Date(),
+        },
+      ]);
+    }
+
+    setCurrentMessage('');
+  };
 
   return (
     <>
       <Container>
+        <InputView>
+          <UserInput onChangeText={setCurrentMessage} value={currentMessage} />
+          <MessageSend onPress={sendMessage}>
+            <Icon name="send" size={28} style={{ left: -1, top: 1 }} />
+          </MessageSend>
+        </InputView>
+
         <FlatList
           data={messages}
           showsVerticalScrollIndicator={false}
-          renderItem={({ item: message }) => (
-            <MessageView sender={message.sender}>
-              <MessageText>{message.body}</MessageText>
-              <MessageTime>{format(message.time, 'HH:mm')}</MessageTime>
-            </MessageView>
+          renderItem={({ item: message, index }) => (
+            <>
+              <MessageView sender={message.sender} style={index === messages.length - 1 && { marginBottom: 0 }}>
+                <MessageText>{message.body}</MessageText>
+                <MessageTime>{format(message.time, 'HH:mm')}</MessageTime>
+              </MessageView>
+              {Object.keys(dates).includes(message.id) && (
+                <DateSection>
+                  <DateSectionMark />
+                  <DateSectionText>{dates[message.id]}</DateSectionText>
+                </DateSection>
+              )}
+            </>
           )}
           inverted
-          contentContainerStyle={{ flexDirection: 'column-reverse' }}
+          contentContainerStyle={{ flexDirection: 'column-reverse', paddingBottom: 12 }}
         />
       </Container>
 
--- mobile/src/ui/styles/Chat.ts
+++ mobile/src/ui/styles/Chat.ts
@@ -19,6 +19,7 @@ export const MessageView = styled.View<MessageProps>`
   border-radius: 12px;
   padding: 8px;
   margin-bottom: 12px;
+
   ${({ sender }) =>
     sender === 'system'
       ? {
@@ -40,4 +41,55 @@ export const MessageTime = styled.Text`
   align-self: flex-end;
 
   font-size: 12px;
+`;
+
+export const DateSection = styled.View`
+  align-items: center;
+  position: relative;
+
+  margin: -4px 0 4px;
+`;
+
+export const DateSectionMark = styled.View`
+  position: absolute;
+  top: 10px;
+
+  width: 100%;
+  height: 1px;
+  background-color: #0005;
+`;
+
+export const DateSectionText = styled.Text`
+  background-color: #c4d3f2;
+`;
+
+export const InputView = styled.View`
+  flex-direction: row;
+
+  width: 100%;
+  height: 40px;
+
+  margin: 12px 0 16px;
+`;
+
+export const UserInput = styled.TextInput`
+  flex: 1;
+
+  background-color: #fff;
+  color: #000;
+
+  border-radius: 30px;
+  padding: 0 12px;
+  margin-left: 16px;
+`;
+
+export const MessageSend = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+
+  width: 40px;
+  background-color: #8099ceaa;
+
+  border-radius: 20px;
+  margin: 0 16px;
 `;

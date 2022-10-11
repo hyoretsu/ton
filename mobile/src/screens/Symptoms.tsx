e5d36ccd5243443996d7ad3459e33c6ede793da9
Aran Leite <hyoretsu@gmail.com>
2022-10-11 13:18:05 -0300

    Remake do formulário de sintomatologia

--- mobile/src/screens/Symptoms.tsx
+++ mobile/src/screens/Symptoms.tsx
@@ -1,8 +1,8 @@
 import { useStorage } from '@contexts/storage';
 import { useNavigation } from '@react-navigation/native';
-import { range } from 'data/utils';
-import { useState } from 'react';
-import { Platform } from 'react-native';
+import { useRef, useState } from 'react';
+import { Platform, View } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 import Button from '@components/Button';
 
@@ -13,8 +13,10 @@ import {
   Info,
   Selection,
   SelectionCircle,
+  SelectionColumn,
+  SelectionInput,
   SelectionItem,
-  SelectionNumber,
+  SelectionRow,
   SelectionText,
   Symptom,
   SymptomQuestion,
@@ -25,18 +27,30 @@ const Symptoms: React.FC = () => {
   const { navigate } = useNavigation();
   const { checkupProgress, storeValue } = useStorage();
 
-  const [symptomAnswers, setAnswers] = useState<Record<string, number>>({});
+  const symptomInputRef = useRef<TextInput>(null);
+  const [answers, setAnswers] = useState<Record<string, string>>({});
+  const [question2Input, setQuestion2Input] = useState('');
 
-  const symptomQuestions = [
-    'Consegue ingerir alimentos sólidos?',
-    'Consegue ingerir líquidos?',
-    'Sente dor ao engolir?',
-    'Sente dor ao mastigar?',
-    'A boca está hidratada?',
+  const questions = [
+    'O paciente sente ou sentiu dor na boca esta semana?',
+    'Se sim, onde foi a dor?',
+    'O paciente está se alimentando normal?',
+    'O paciente reclamou de sentir a boca mais seca? Com menos saliva que o normal?',
+    'O paciente percebe que seus lábios estão ressecados?',
+  ];
+
+  const painOptions = ['Garganta', 'Dente', 'Língua', 'Gengiva', 'Lábio'];
+
+  const eatingOptions = [
+    'Sim',
+    'Não, está com fastio.',
+    'Não, só líquidos, como sucos, Sustagen e vitaminas.',
+    'Não, só comida pastosa.',
+    'Não, nem líquidos consegue tomar.',
   ];
 
   const finishCheckup = async (): Promise<void> => {
-    if (Object.values(symptomAnswers).length < 5) {
+    if (Object.entries(answers).length < (Object.values(answers)[0] === 'Sim' ? 5 : 4)) {
       return;
     }
 
@@ -50,7 +64,7 @@ const Symptoms: React.FC = () => {
         name: (path.match(/mrousavy.*\.jpg/) as string[])[0],
       }),
     );
-    formData.append('answers', JSON.stringify(symptomAnswers));
+    formData.append('answers', JSON.stringify(answers));
 
     await api.post('/checkup', formData, {
       headers: {
@@ -66,28 +80,182 @@ const Symptoms: React.FC = () => {
   return (
     <Container>
       <Title>Sintomatologia</Title>
-      <Info>Agora, algumas perguntas para sabermos se está tudo bem.{'\n'}O paciente:</Info>
-      {symptomQuestions.map((symptom, index) => (
-        <Symptom key={index}>
-          <SymptomQuestion>{symptom}</SymptomQuestion>
+      <Info>
+        Obrigada pelas fotos 😊{'\n\n'}
+        Para finalizar o exame desta semana, pedimos que responda as próximas questões sobre como a criança/adolescente
+        está se sentindo:
+      </Info>
+      <Symptom>
+        <SymptomQuestion>{questions[0]}</SymptomQuestion>
+        <Selection>
+          <SelectionItem>
+            <SelectionCircle
+              onPress={() => setAnswers(old => ({ ...old, [questions[0]]: 'Sim' }))}
+              selected={answers[questions[0]] === 'Sim'}
+            />
+            <SelectionText>Sim</SelectionText>
+          </SelectionItem>
+          <SelectionItem>
+            <SelectionCircle
+              onPress={() => setAnswers(old => ({ ...old, [questions[0]]: 'Não' }))}
+              selected={answers[questions[0]] === 'Não'}
+            />
+            <SelectionText>Não</SelectionText>
+          </SelectionItem>
+        </Selection>
+      </Symptom>
+      {answers[questions[0]] === 'Sim' && (
+        <Symptom>
+          <SymptomQuestion>{questions[1]}</SymptomQuestion>
           <Selection>
-            <SelectionText>Pouco</SelectionText>
-            {range(1, 6).map(position => (
-              <SelectionItem key={position}>
+            <SelectionColumn>
+              <SelectionItem horizontal>
                 <SelectionCircle
-                  onPress={() => {
-                    setAnswers(old => ({ ...old, [symptom]: position }));
-                  }}
-                  selected={symptomAnswers[symptom] === position}
+                  onPress={() => setAnswers(old => ({ ...old, [questions[1]]: painOptions[0] }))}
+                  selected={answers[questions[1]] === painOptions[0]}
                 />
-                <SelectionNumber>{position}</SelectionNumber>
+                <SelectionText>{painOptions[0]}</SelectionText>
               </SelectionItem>
-            ))}
-            <SelectionText>Muito</SelectionText>
+              <SelectionItem horizontal>
+                <SelectionCircle
+                  onPress={() => setAnswers(old => ({ ...old, [questions[1]]: painOptions[1] }))}
+                  selected={answers[questions[1]] === painOptions[1]}
+                />
+                <SelectionText>{painOptions[1]}</SelectionText>
+              </SelectionItem>
+              <SelectionItem horizontal>
+                <SelectionCircle
+                  onPress={() => setAnswers(old => ({ ...old, [questions[1]]: painOptions[2] }))}
+                  selected={answers[questions[1]] === painOptions[2]}
+                />
+                <SelectionText>{painOptions[2]}</SelectionText>
+              </SelectionItem>
+            </SelectionColumn>
+            <SelectionColumn>
+              <SelectionItem horizontal>
+                <SelectionCircle
+                  onPress={() => setAnswers(old => ({ ...old, [questions[1]]: painOptions[3] }))}
+                  selected={answers[questions[1]] === painOptions[3]}
+                />
+                <SelectionText>{painOptions[3]}</SelectionText>
+              </SelectionItem>
+              <SelectionItem horizontal>
+                <SelectionCircle
+                  onPress={() => setAnswers(old => ({ ...old, [questions[1]]: painOptions[4] }))}
+                  selected={answers[questions[1]] === painOptions[4]}
+                />
+                <SelectionText>{painOptions[4]}</SelectionText>
+              </SelectionItem>
+              <SelectionItem horizontal>
+                <SelectionCircle
+                  onPress={() => symptomInputRef.current?.focus()}
+                  selected={!!answers[questions[1]] && !painOptions.includes(answers[questions[1]])}
+                />
+                <SelectionInput
+                  ref={symptomInputRef}
+                  placeholder="Outro..."
+                  value={question2Input}
+                  onChange={e => {
+                    e.persist();
+                    setQuestion2Input(e.nativeEvent.text);
+                    setAnswers(old => ({ ...old, [questions[1]]: e.nativeEvent.text }));
+                  }}
+                />
+              </SelectionItem>
+            </SelectionColumn>
           </Selection>
         </Symptom>
-      ))}
-      <Button onPress={finishCheckup} style={{ marginTop: 12 }}>
+      )}
+      <Symptom>
+        <SymptomQuestion>{questions[2]}</SymptomQuestion>
+        <Selection style={{ flexDirection: 'column' }}>
+          <SelectionItem horizontal style={{ justifyContent: 'center' }}>
+            <SelectionCircle
+              onPress={() => setAnswers(old => ({ ...old, [questions[2]]: eatingOptions[0] }))}
+              selected={answers[questions[2]] === eatingOptions[0]}
+            />
+            <SelectionText>{eatingOptions[0]}</SelectionText>
+          </SelectionItem>
+          <View style={{ alignItems: 'center' }}>
+            <SelectionRow>
+              <SelectionItem horizontal style={{ width: '40%' }}>
+                <SelectionCircle
+                  onPress={() => setAnswers(old => ({ ...old, [questions[2]]: eatingOptions[1] }))}
+                  selected={answers[questions[2]] === eatingOptions[1]}
+                />
+                <SelectionText>{eatingOptions[1]}</SelectionText>
+              </SelectionItem>
+              <SelectionItem horizontal style={{ width: '40%' }}>
+                <SelectionCircle
+                  onPress={() =>
+                    setAnswers(old => ({
+                      ...old,
+                      [questions[2]]: eatingOptions[2],
+                    }))
+                  }
+                  selected={answers[questions[2]] === eatingOptions[2]}
+                />
+                <SelectionText>{eatingOptions[2]}</SelectionText>
+              </SelectionItem>
+            </SelectionRow>
+            <SelectionRow>
+              <SelectionItem horizontal style={{ width: '40%' }}>
+                <SelectionCircle
+                  onPress={() => setAnswers(old => ({ ...old, [questions[2]]: eatingOptions[3] }))}
+                  selected={answers[questions[2]] === eatingOptions[3]}
+                />
+                <SelectionText>{eatingOptions[3]}</SelectionText>
+              </SelectionItem>
+              <SelectionItem horizontal style={{ width: '40%' }}>
+                <SelectionCircle
+                  onPress={() => setAnswers(old => ({ ...old, [questions[2]]: eatingOptions[4] }))}
+                  selected={answers[questions[2]] === eatingOptions[4]}
+                />
+                <SelectionText>{eatingOptions[4]}</SelectionText>
+              </SelectionItem>
+            </SelectionRow>
+          </View>
+        </Selection>
+      </Symptom>
+      <Symptom>
+        <SymptomQuestion>{questions[3]}</SymptomQuestion>
+        <Selection>
+          <SelectionItem>
+            <SelectionCircle
+              onPress={() => setAnswers(old => ({ ...old, [questions[3]]: 'Sim' }))}
+              selected={answers[questions[3]] === 'Sim'}
+            />
+            <SelectionText>Sim</SelectionText>
+          </SelectionItem>
+          <SelectionItem>
+            <SelectionCircle
+              onPress={() => setAnswers(old => ({ ...old, [questions[3]]: 'Não' }))}
+              selected={answers[questions[3]] === 'Não'}
+            />
+            <SelectionText>Não</SelectionText>
+          </SelectionItem>
+        </Selection>
+      </Symptom>
+      <Symptom>
+        <SymptomQuestion>{questions[4]}</SymptomQuestion>
+        <Selection>
+          <SelectionItem>
+            <SelectionCircle
+              onPress={() => setAnswers(old => ({ ...old, [questions[4]]: 'Sim' }))}
+              selected={answers[questions[4]] === 'Sim'}
+            />
+            <SelectionText>Sim</SelectionText>
+          </SelectionItem>
+          <SelectionItem>
+            <SelectionCircle
+              onPress={() => setAnswers(old => ({ ...old, [questions[4]]: 'Não' }))}
+              selected={answers[questions[4]] === 'Não'}
+            />
+            <SelectionText>Não</SelectionText>
+          </SelectionItem>
+        </Selection>
+      </Symptom>
+      <Button onPress={finishCheckup} style={{ marginTop: 12, marginBottom: 32 }}>
         Enviar
       </Button>
     </Container>
--- mobile/src/ui/styles/Symptoms.ts
+++ mobile/src/ui/styles/Symptoms.ts
@@ -1,13 +1,19 @@
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
+
+interface SelectionItemProps {
+  horizontal?: boolean;
+}
 
 interface SelectionCircleProps {
   selected: boolean;
 }
 
-export const Container = styled.View`
-  align-items: center;
-`;
+export const Container = styled(ScrollView).attrs({
+  contentContainerStyle: {
+    alignItems: 'center',
+  },
+})``;
 
 export const Title = styled.Text`
   font-size: 32px;
@@ -29,6 +35,9 @@ export const Info = styled.Text`
 `;
 
 export const Symptom = styled.View`
+  width: 100%;
+  align-items: center;
+
   margin-bottom: 16px;
 `;
 
@@ -43,10 +52,20 @@ export const SymptomQuestion = styled.Text`
 
 export const Selection = styled.View`
   flex-direction: row;
+  justify-content: center;
 
   width: 100%;
 
   padding: 0 8px;
+`;
+
+export const SelectionColumn = styled.View``;
+
+export const SelectionRow = styled.View`
+  flex-direction: row;
+  justify-content: space-evenly;
+
+  margin-right: 20px;
 `;
 
 export const SelectionText = styled.Text`
@@ -54,10 +73,21 @@ export const SelectionText = styled.Text`
   color: #52609d;
 `;
 
-export const SelectionItem = styled.View`
+export const SelectionInput = styled(TextInput).attrs({ placeholderTextColor: '#52609d' })`
+  max-width: 60px;
+  font-size: 16px;
+  color: #52609d;
+
+  padding: 0;
+`;
+
+export const SelectionItem = styled.View<SelectionItemProps>`
+  ${({ horizontal }) => horizontal && { flexDirection: 'row' }}
   align-items: center;
 
-  margin: 0 auto;
+  ${({ horizontal }) => horizontal && { width: '100%' }}
+
+  margin: 0 12px ${({ horizontal }) => (horizontal ? 8 : 0)}px;
 `;
 
 export const SelectionCircle = styled(TouchableOpacity)<SelectionCircleProps>`
@@ -65,10 +95,7 @@ export const SelectionCircle = styled(TouchableOpacity)<SelectionCircleProps>`
   height: 20px;
   background-color: ${({ selected }) => (selected ? '#889ae8' : '#c4d3f2')};
 
+  margin: 0 8px;
   border: ${({ selected }) => (selected ? 0 : 2)}px #889ae8;
   border-radius: 10px;
-`;
-
-export const SelectionNumber = styled.Text`
-  color: #52609d;
 `;

983d8c76623e1c19c4247e5ccd2d522e591ea3fd
Aran Leite <hyoretsu@gmail.com>
2023-03-25 23:27:32 -0300

    Mobile: Added Expo icons and splash screen
    Plus app.config.ts minor fixes

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -1,16 +1,27 @@
 export default {
     expo: {
-        name: 'TON',
+        android: {
+            adaptiveIcon: {
+                backgroundColor: '#eaf7f9',
+                foregroundImage: './src/assets/adaptive-icon.png',
+            },
+        },
+        icon: './src/assets/icon.png',
+        name: 'Ton',
         plugins: [
             [
                 'expo-camera',
                 {
-                    cameraPermission: 'Allow $(PRODUCT_NAME) to access your camera.',
+                    cameraPermission: 'Conceda acesso à câmera para $(PRODUCT_NAME).',
                 },
             ],
         ],
         scheme: 'ton',
         slug: 'ton',
+        splash: {
+            image: './src/assets/splash.png',
+            backgroundColor: '#eaf7f9',
+        },
         version: '1.0.0',
     },
 };

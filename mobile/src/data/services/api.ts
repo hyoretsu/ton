6c0c12a9ab8e061cc5de7e99c7b064ab13e9f983
Aran Leite <hyoretsu@gmail.com>
2023-06-30 17:49:57 -0300

    "Definitive" fix for mobile checkup

--- mobile/src/data/services/api.ts
+++ mobile/src/data/services/api.ts
@@ -2,8 +2,8 @@ import { API_URL } from '@env';
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://odontologiadigital.ccs.ufpb.br/ton',
-    baseURL: API_URL,
+    baseURL: 'https://odontologiadigital.ccs.ufpb.br/ton',
+    // baseURL: API_URL,
 });
 
 export default api;
